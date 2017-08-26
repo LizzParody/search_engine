f96af18366fd950c61fc8fc1e0bba1850183fb47
Liz Parody <lizparody@Lizs-MacBook-Air.local>
2017-08-25 19:15:36 -0500

    Finished the Search Engine

--- search_engine.py
+++ search_engine.py
@@ -1,3 +1,4 @@
+# This is the cache of the few web pages I'm going to do do a search and get the best result
 cache = {
    'http://udacity.com/cs101x/urank/index.html': """<html>
 <body>
@@ -115,3 +116,135 @@ Hummus Recipe
 
 """,
 }
+
+def get_page(url):
+    if url in cache:
+        return cache[url]
+    return ""
+
+# How I get the first link and the rest of the page
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+# Getting all the links of a web page in the list 'links'
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+# To not repeat links
+def union(a, b):
+    for e in b:
+        if e not in a:
+            a.append(e)
+
+# Creating the index as a dictionary
+def add_to_index(index, keyword, url):
+    if keyword in index:              # If the keyword is already in the index, append the url
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]        # If the keyword is not in the index, create a new keyword with the url
+
+# Add all the keywords in a webpage in the index
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+# Return the url with a given keyword
+def lookup(index, keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
+
+# Creating the web crawl
+def crawl_web(seed): # returns index, graph of inlinks
+    tocrawl = [seed]
+    crawled = []
+    graph = {}  # <url>, [list of pages it links to]
+    index = {}
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            outlinks = get_all_links(content)
+            graph[page] = outlinks
+            union(tocrawl, outlinks)
+            crawled.append(page)
+    return index, graph
+
+# Giving ranks to the webpage
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+
+# Return a list of urls depending on the highest ranks
+def ordered_search(index, ranks, keyword):
+    pages = lookup(index, keyword)
+    return quicksort(pages, ranks)
+
+# Organizing the links using the quicksort algorithm, according to the highest ranks
+def quicksort(pages, ranks):
+    if not pages or len(pages) <= 1:
+        return pages
+    else:
+        pivot = ranks[pages[0]]
+        worse = []
+        better = []
+        for page in pages[1:]:
+            if ranks[page] <= pivot:
+                worse.append(page)
+            else:
+                better.append(page)
+        return quicksort(better, ranks) + [pages[0]] + quicksort(worse, ranks)
+
+index, graph = crawl_web('http://udacity.com/cs101x/urank/index.html')
+ranks = compute_ranks(graph)
+
+# TESTING IF THE SEARCH ENGINE WORKS:
+print(ordered_search(index, ranks, 'Hummus'))
+#>>> ['http://udacity.com/cs101x/urank/kathleen.html',
+#    'http://udacity.com/cs101x/urank/nickel.html',
+#    'http://udacity.com/cs101x/urank/arsenic.html',
+#    'http://udacity.com/cs101x/urank/hummus.html',
+#    'http://udacity.com/cs101x/urank/index.html']
+
+print(ordered_search(index, ranks, 'the'))
+#>>> ['http://udacity.com/cs101x/urank/nickel.html',
+#    'http://udacity.com/cs101x/urank/arsenic.html',
+#    'http://udacity.com/cs101x/urank/hummus.html',
+#    'http://udacity.com/cs101x/urank/index.html']
+
+
+print(ordered_search(index, ranks, 'babaganoush'))
+#>>> None
